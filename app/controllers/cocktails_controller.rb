8b33b36f3a37e29ff65cf09ebcd01c3246bfa83b
yutian zheng <yutian.zheng@mail.mcgill.ca>
2017-07-08 18:57:48 -0400

    cocktails controller and routes added

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,41 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktails_path
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    respond_to do |format|
+      if @cocktail.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:show, :index, :new, :create, :destroy] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
+  resources :ingredients, only: [:show]
+
+  root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
