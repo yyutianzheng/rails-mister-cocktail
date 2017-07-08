07489931fba9457dd0cc077afd5ffd1e0f4f756a
yutian zheng <yutian.zheng@mail.mcgill.ca>
2017-07-08 19:31:17 -0400

    cocktails index.html.erb show.html.erb created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Cocktail.create(name: 'Martini')
+  Cocktail.create(name: 'Vodka')
+  Cocktail.create(name: 'Tequila')
+  Cocktail.create(name: 'Mart')
+  Ingredient.create([{name: 'ice'}, {name: 'juice'} ])
